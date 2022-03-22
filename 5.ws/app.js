a38cfa559935861345446ca61beb136bbf08f4cd
wanglin <wanglin46@lenovo.com>
2022-03-22 14:50:14 +0800

    feat:ws使用

---
+++ 5.ws/app.js
@@ -0,0 +1,41 @@
+let express = require('express')
+let app = express()
+
+app.use(express.static(__dirname))
+app.listen(3000)
+
+const DATA = {
+  'a': {
+    data: ['a']
+  },
+  'q': {
+    data: ['q']
+  },
+  'w': {
+    data: ['w']
+  },
+  'e': {
+    data: ['e']
+  },
+  'r': {
+    data: ['r']
+  },
+  't': {
+    data: ['t']
+  },
+  'y': {
+    data: ['y']
+  }
+}
+
+let WebSocketServer = require('ws').Server
+let server = new WebSocketServer({ port: 8888 })
+// socket 插座
+server.on('connection', function (socket) {
+  console.log('2.服务器端监听到了客户端的链接')
+  // 监听客户端发过来的消息
+  socket.on('message', function (message) {
+    console.log('4.客户端发过来的消息', message)
+    socket.send('5.服务器说:' + JSON.stringify(DATA))
+  })
+})
